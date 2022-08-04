e3a88e4988956d202599cbb446d2bc4d3ebff3d4
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-04 22:14:47 +0900

    [feat]각 페이지 생성 및 기본 세팅

--- cameet/src/App.js
+++ cameet/src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./routes/Home";
+import LoginPage from "./routes/LoginPage";
+import Info from "./routes/Info";
+import Landing from "./routes/Landing";
+import MyPage from "./routes/MyPage";
+import RoomCreate from "./routes/RoomCreate";
+import RoomDetail from "./routes/RoomDetail";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ cameet/src/routes/RoomCreate.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoomCreate = () => {
+  return (
+    <div>RoomCreate</div>
+  )
+}
+
+export default RoomCreate
