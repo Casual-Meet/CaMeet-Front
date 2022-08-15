59660a74b6c44a1869aaef40aadd3eddaa468d28
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-16 05:22:45 +0900

    [feat]roomcreate input

--- cameet/src/routes/RoomCreate.js
+++ cameet/src/routes/RoomCreate.js
@@ -1,19 +1,76 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import Nav from "../components/common/Nav";
 import styled from "styled-components";
-import { Title, SubTitle, DefaultButton, Layout } from "../utils/styles";
+import { Title, SubTitle, DefaultButton, Layout, Input } from "../utils/styles";
 import Terms from "../components/common/Terms";
-
-
+import axios from "axios";
 
 const RoomCreate = () => {
+  const [roomTitle, setRoomTitle] = useState("");
+  const [roomInterest, setRoomInterest] = useState("");
+
+  const onChangeRoomTitle = useCallback((e) => {
+    setRoomTitle(e.target.value);
+  }, []);
+  const onChangeRoomInterest = useCallback((e) => {
+    setRoomInterest(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(roomTitle);
+
+      axios
+        .post("http://localhost:8000/room/create/", {
+          headers: {
+            "Content-type": "application/json",
+          },
+          withCredentials: true,
+        })
+        .then((res) => res.json())
+        .then((data) => {
+          setRoomTitle(data.data.room_title);
+          setRoomInterest(data.data.room_interest);
+        })
+        .catch((error) => {
+          setRoomTitle("");
+          setRoomInterest("");
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    [roomTitle]
+  );
+
   return (
     <>
       <Nav />
       <Layout>
         <Title>모임을 시작해보세요</Title>
-        <Terms />
-        <DefaultButton>생성하기</DefaultButton>
+        <form onSubmit={onSubmit}>
+          <SubTitle>방 제목</SubTitle>
+          <div>
+            <Input
+              type="text"
+              name="roomtitle"
+              value={roomTitle}
+              onChange={onChangeRoomTitle}
+            />
+          </div>
+          <SubTitle>관심사</SubTitle>
+          <SubTitle>모임 날짜</SubTitle>
+          <SubTitle>만남 장소</SubTitle>
+          <SubTitle>모임 정원</SubTitle>
+          <Terms />
+          <DefaultButton
+            type="submit"
+            onClick={() => alert("방이 생성되었습니다.")}
+          >
+            생성하기
+          </DefaultButton>
+        </form>
       </Layout>
     </>
   );
