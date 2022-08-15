87ec7a12779dd6b318079fa5e07c2d64df272365
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-16 04:07:23 +0900

    [feat] mypage axios 연결

---
+++ cameet/src/api/mypageAPI.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import React from 'react';
+
+const mypageAPI = axios.create({
+    baseURL : "http://localhost:8000/accounts/mypage",
+    headers : {
+        "Content-Type" : "application/json",
+    },
+    withCredentials:true,
+})
+
+export default mypageAPI;
--- cameet/src/routes/MyPage.js
+++ cameet/src/routes/MyPage.js
@@ -1,12 +1,29 @@
-import React from 'react'
-import userData from '../db/userData';
+import React, { useState } from "react";
+import { Title, SubTitle, DefaultButton, Layout } from "../utils/styles";
+import Nav from "../components/common/Nav";
+import mypageAPI from "../api/mypageAPI";
+import imageInput from '../components/mypage/imageInput';
 
 const MyPage = () => {
+  const []
   return (
     <>
-      
+      <Nav />
+      <Layout>
+        <Title>내 프로필</Title>
+        <imageInput />
+        <SubTitle>학교 이메일</SubTitle>
+        <SubTitle>이메일(ID)</SubTitle>
+        <SubTitle>비밀번호</SubTitle>
+        <SubTitle>전화번호</SubTitle>
+        <SubTitle>이름</SubTitle>
+        <SubTitle>닉네임</SubTitle>
+        <SubTitle>관심사</SubTitle>
+        <SubTitle>전화번호</SubTitle>
+        <SubTitle>MBTI</SubTitle>
+      </Layout>
     </>
-  )
-}
+  );
+};
 
-export default MyPage
+export default MyPage;
