60bdd8cd5274995ee8d8572ee987514d5c5f2f62
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-16 18:33:23 +0900

    [feat] 모임날짜/시간까지 반응형 기능

--- cameet/src/components/roomcreate/kakaomap.js
+++ cameet/src/components/roomcreate/kakaomap.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const kakaomap = () => {
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     );
 };
 
--- cameet/src/routes/RoomCreate.js
+++ cameet/src/routes/RoomCreate.js
@@ -4,12 +4,15 @@ import styled from "styled-components";
 import { Title, SubTitle, DefaultButton, Layout, Input } from "../utils/styles";
 import Terms from "../components/common/Terms";
 import axios from "axios";
+import kakaomap from "../components/roomcreate/kakaomap";
 
 const RoomCreate = () => {
   const [roomTitle, setRoomTitle] = useState("");
   const [roomInterest, setRoomInterest] = useState("");
   const [roomdate, setRoomDate] = useState("");
   const [roomtime, setRoomTime] = useState("");
+  // 로딩 상태
+  const [loading, setLoading] = useState(false);
   // 중복 제출 방지
   const [disabled, setDisabled] = useState(false);
 
@@ -19,38 +22,42 @@ const RoomCreate = () => {
   const onChangeRoomInterest = useCallback((e) => {
     setRoomInterest(e.target.value);
   }, []);
+  const onChangeRoomDate = useCallback((e) => {
+    setRoomDate(e.target.value);
+  }, []);
+  const onChangeRoomTime = useCallback((e) => {
+    setRoomTime(e.target.value);
+  }, []);
 
-  const onSubmit = useCallback(
-    (e) => {
-      setDisabled(true);
-      e.preventDefault();
-      console.log(roomTitle);
-      alert("방이 생성되었습니다.");
+  const onSubmit = useCallback((e) => {
+    setDisabled(true);
+    e.preventDefault();
+    setLoading(true);
+    // alert("방이 생성되었습니다.");
 
-      axios
-        .post("http://localhost:8000/room/create/", {
-          headers: {
-            "Content-type": "application/json",
-          },
-          withCredentials: true,
-        })
-        .then((res) => res.json())
-        .then((data) => {
-          setRoomTitle(data.data.room_title);
-          setRoomInterest(data.data.room_interest);
-        })
-        .catch((error) => {
-          setRoomTitle("");
-          setRoomInterest("");
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      setDisabled(true);
-    },
-    [roomTitle]
-  );
+    axios
+      .post("http://localhost:8000/room/create/", {
+        headers: {
+          "Content-type": "application/json",
+        },
+        withCredentials: true,
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setRoomTitle(data.data.room_title);
+        setRoomInterest(data.data.room_interest);
+      })
+      .catch((error) => {
+        setRoomTitle("");
+        setRoomInterest("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    alert("방이 생성되었습니다.");
+    setLoading(false);
+    setDisabled(true);
+  });
 
   return (
     <>
@@ -81,11 +88,25 @@ const RoomCreate = () => {
               required
             />
           </div>
-          <SubTitle>모임 날짜</SubTitle>
-          <Input type="date" min="2021-01-01" max="2050-01-01" name="date" />
-          <Input type="time" name="time" />
+          <SubTitle>모임 날짜/시간</SubTitle>
+          <Input
+            type="date"
+            value={roomdate}
+            onChange={onChangeRoomDate}
+            min="2021-01-01"
+            max="2050-01-01"
+            name="date"
+          />
+          <Input
+            type="time"
+            value={roomtime}
+            onChange={onChangeRoomTime}
+            name="time"
+          />
           <SubTitle>만남 장소</SubTitle>
+          <kakaomap />
           <SubTitle>모임 정원</SubTitle>
+
           <Terms />
           <DefaultButton type="submit" disabled={disabled}>
             생성하기
