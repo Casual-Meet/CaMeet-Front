e3ab1212830463d17e335d6c7727fb7306c1a3e5
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-20 00:52:33 +0900

    [feat] info slider             가

--- cameet/src/routes/Info.js
+++ cameet/src/routes/Info.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState, useCallback } from "react";
+import axios from "axios";
+import { Title, SubTitle, Layout } from "../utils/styles";
+import 'animate.css/animate.min.css'
 
 const Info = () => {
   return (
-    <div>Info</div>
-  )
-}
+    <>
+      <div>여기는 info 페이지입니다.</div>
+    </>
+  );
+};
 
-export default Info
+export default Info;
