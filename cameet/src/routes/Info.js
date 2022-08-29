33b34db6149570df48aab8714d61e26ef8614131
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-08-29 23:09:34 +0900

    [feat] info 방 리스트

--- cameet/src/routes/Info.js
+++ cameet/src/routes/Info.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import axios from "axios";
 import { Title, SubTitle, Layout } from "../utils/styles";
 import "animate.css/animate.min.css";
@@ -6,13 +6,33 @@ import styled from "styled-components";
 
 const InfoWrapper = styled.div`
   margin: 2vw 2vw;
-`
+`;
+
+const Loader = styled.div`
+
+`;
 
 const Info = () => {
+  const [makeRoom, setMakeRoom] = useState([]);
+  const [goRoom, setGoRoom] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // 시작할 때 1번만 실행
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("");
+      const json = await response.json();
+
+      setLoading(false);
+    })();
+  }, []);
+  console.log(makeRoom);
   return (
     <>
       <InfoWrapper>
         <Title>내 정보</Title>
+        {loading ? (
+          <Loader>loading</Loader>
+        ):()}
       </InfoWrapper>
     </>
   );
