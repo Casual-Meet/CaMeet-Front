b328b581d1c9d006083a8da11f28cb8fd29bbf24
d0422 <rlfehd2013@naver.com>
2022-09-07 11:09:46 +0900

    :zap: getdays 날짜 수정, 날짜별 state 설정

--- cameet/src/functions/getDays.js
+++ cameet/src/functions/getDays.js
@@ -4,18 +4,18 @@ export function getDays(day) {
   console.log(newday);
   switch (newday.getDay()) {
     case 1:
-      return "화";
-    case 2:
-      return "수";
-    case 3:
-      return "목";
-    case 4:
-      return "금";
-    case 5:
-      return "토";
-    case 6:
-      return "일";
-    default:
       return "월";
+    case 2:
+      return "화";
+    case 3:
+      return "수";
+    case 4:
+      return "목";
+    case 5:
+      return "금";
+    case 6:
+      return "토";
+    default:
+      return "일";
   }
 }
---
+++ cameet/src/functions/isToday.js
@@ -0,0 +1,15 @@
+export default function isToday(day) {
+  const args = day.split("-");
+  const today = new Date();
+  const year = today.getFullYear();
+  // 10보다 작은 경우 0을 앞에 붙여줌
+  const month =
+    today.getMonth() < 10 ? "0" + today.getMonth() : today.getMonth();
+  // 10보다 작은 경우 0을 앞에 붙여줌
+  const d = today.getDate() < 10 ? "0" + today.getDate() : today.getDate();
+  const todayarr = [String(year), month, d];
+  if (JSON.stringify(args) === JSON.stringify(todayarr)) {
+    return true;
+  }
+  return false;
+}
