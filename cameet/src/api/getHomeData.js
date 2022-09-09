67b9c9104936ab0cd4a06341910d11ee0e139383
d0422 <rlfehd2013@naver.com>
2022-09-09 20:26:41 +0900

    :zap: detail페이지 ui 개선, home페이지 api 변경

--- cameet/src/api/getHomeData.js
+++ cameet/src/api/getHomeData.js
@@ -4,11 +4,8 @@ import { BASE_URL } from "./BaseURL";
 // 홈화면 api
 // 받아온 데이터를 날짜별 배열을 가진 객체로 분리
 export default function getHomeData() {
-  fetch(`${BASE_URL}aweek-roomlist/`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
   return axios
-    .get(`${BASE_URL}aweek-roomlist/`)
+    .get(`${BASE_URL}`)
     .then((res) => res.data)
     .then((data) => divideRoom(data));
 }
--- cameet/src/routes/RoomDetail.js
+++ cameet/src/routes/RoomDetail.js
@@ -1,18 +1,34 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
+import styled from "styled-components";
 import getRoomDetail from "../api/getRoomDetail";
 import Nav from "../components/common/Nav";
+import Terms from "../components/common/Terms";
+import { getDays } from "../functions/getDays";
 
 const RoomDetail = () => {
   const { id } = useParams();
   const { data, isLoading } = useQuery(["detail", id], () => getRoomDetail(id));
+  const day = data && getDays(data.room_date);
   console.log(data);
   return (
     <>
       <Nav />
+      <div>
+        <Time>
+          <div>{data?.room_date}</div>
+          <div>{day}요일</div>
+          <div>{data?.room_time.substr(0, 5)}</div>
+        </Time>
+        <div>{data?.room_title}</div>
+      </div>
+      <hr></hr>
+      <Terms></Terms>
     </>
   );
 };
-
+const Time = styled.div`
+  display: flex;
+`;
 export default RoomDetail;
