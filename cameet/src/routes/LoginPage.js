6a288c8a4c73ad170cccb25aac227cde9756ef21
Seojisoo20191941 <amy0480@naver.com>
2022-09-11 01:53:22 +0900

    feat:add kakao and google login logo

--- cameet/src/routes/LoginPage.js
+++ cameet/src/routes/LoginPage.js
@@ -1,15 +1,27 @@
 import React from "react";
-import logoImg from "../images/cameetlogo.png";
+import cameetLogo from "../images/cameetlogo.png";
+import kakaoLogin from "../images/kakaologin.png";
+import googleLogin from "../images/googlelogin.png";
 import styled from "styled-components";
+import { Layout } from "../utils/styles";
+
+const Dom = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+`;
 
 const Logo = styled.img`
-  width: 100%;
+  width: 80%;
 `;
 const LoginPage = () => {
   return (
-    <>
-      <Logo src={logoImg} alt="#" />
-    </>
+    <Layout>
+      <Logo src={cameetLogo} alt="#" />
+      <Logo src={kakaoLogin} alt="#" />
+      <Logo src={googleLogin} alt="#" />
+    </Layout>
   );
 };
 
