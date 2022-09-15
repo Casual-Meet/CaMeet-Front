453819ffef9ebfe0921a48c8839586091f7d9dd9
Seojisoo20191941 <amy0480@naver.com>
2022-09-16 00:49:40 +0900

    feat:git

--- cameet/src/db/secret.js
+++
@@ -1,9 +0,0 @@
-export const SOCIAL_AUTH_GOOGLE_CLIENT_ID =
-  "563502599307-5bpei0i0bj03mqg8u5l975vqsijuj2c7.apps.googleusercontent.com";
-export const SOCIAL_AUTH_GOOGLE_SECRET = "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";
-// const STATE = "STATE";
-export const scope = "https://www.googleapis.com/auth/userinfo.email";
-
-export const API_BASE_URL = "https://cameet.site";
-
-export const OAUTH2_REDIRECT_URI = "http://localhost:3000/";
