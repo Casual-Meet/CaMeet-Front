faf2b3c78d7fb8c53b8074667410ecc8cea61392
Seojisoo20191941 <amy0480@naver.com>
2022-09-20 03:13:17 +0900

    feat:add colors

--- cameet/src/utils/colors.js
+++ cameet/src/utils/colors.js
@@ -1,8 +1,9 @@
 export const COLOR = {
   black: "#000000",
   white: "#fff",
-  mainColor : "#07CA4A",
-  blue : "#0711F8",
-  red : "#E82626",
+  mainColor: "#07CA4A",
+  blue: "#0711F8",
+  red: "#E82626",
   yellow: "#FDF7C6",
+  grey: "#4C4C4C",
 };
