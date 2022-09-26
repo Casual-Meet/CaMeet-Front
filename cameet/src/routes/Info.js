4352d24eeeae6b2eba3eb82c86033313fcfa4379
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-09-26 15:32:07 +0900

    info 뭔가 커밋 충돌나서 풀받았더니 다 없어졌던 사건에 대해

--- cameet/src/routes/Info.js
+++ cameet/src/routes/Info.js
@@ -15,22 +15,55 @@ const InfoWrapper = styled.div`
 const Loader = styled.div``;
 
 const Info = () => {
-  const [makeRoom, setMakeRoom] = useState([]);
-  const [goRoom, setGoRoom] = useState([]);
-  const [loading, setLoading] = useState(true);
-  // 시작할 때 1번만 실행
-  useEffect(() => {
-    (async () => {
-      const response = await fetch("");
-      const json = await response.json();
+  const user = useRecoilValue(session);
+  const token = user.access_token;
+  const { isLoading, error, data, isFetching } = useQuery("userinfo", () =>
+    fetch("https://cameet.site/userinfo/").then((res) => {
+      console.log(res.data);
+    })
+  );
 
-      setLoading(false);
-    })();
-  }, []);
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  // 시작할 때 1번만 실행
+  // useEffect(() => {
+  //   axios
+  //   .get("https://cameet.site/userinfo/", {
+  //     headers:{
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //     "Content-Type" : "application/json",
+  //   })
+  //   .then((res) => {
+  //     console.log(res.data);
+  //     setUserInfo(res.data);
+  //     setSessionData(res.data);
+  //     window.localStorage.setItem("access_token", res.data.access_token);
+  //     window.localStorage.setItem("refresh_token", res.data.refresh_token);
+  //     console.log("돼써");
+  //   });
+  // }, []);
+  // console.log(sessionData);
+
   return (
     <>
       <InfoWrapper>
         <Title>내 정보</Title>
+        <div>{data?.nickname}님</div>
+        <div>
+          <span>{data?.user_keyword1}</span>
+          <span>{data?.user_keyword2}</span>
+        </div>
+        <div>
+          <SubTitle>내가 만든 방</SubTitle>
+          <div>{data?.user_create_rooms.room_title}</div>
+          <div>{data?.user_create_rooms.room_interest}</div>
+        </div>
+        <div>
+          <SubTitle>내가 참여하는 방</SubTitle>
+          <div>{data?.user_join_rooms.room_title.room_interest}</div>
+        </div>
       </InfoWrapper>
     </>
   );
